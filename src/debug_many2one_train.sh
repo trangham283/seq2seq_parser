daebae302acff76aa8b09b29ed5549f58deca5c2
Trang Tran <ttran@malamute.ttic.edu>
2016-08-30 20:23:08 -0500

    Incorporated tcmalloc stuff

--- src/debug_many2one_train.sh
+++ src/debug_many2one_train.sh
@@ -2,6 +2,7 @@
 
 #DATA_DIR=/scratch/ttran/swbd_data 
 DATA_DIR=/tmp/ttran/swbd_data
+TCMALLOC_PATH=/home-nfs/ttran/sw/opt/lib/
 
 if [ ! -e ${DATA_DIR} ]                                                         
 then  
@@ -19,8 +20,12 @@ TRAIN_DIR="/home-nfs/ttran/transitory/speech-nlp/venv_projects/seq2seq_parser/tm
 # using default parameters
 #python debug_many2one.py --data_dir ${DATA_DIR}  --train_dir ${TRAIN_DIR} 
 #python debug_normal.py --data_dir ${DATA_DIR}  --train_dir ${TRAIN_DIR} 
-python debug_many2one.py --data_dir ${DATA_DIR}  --train_dir ${TRAIN_DIR} \
-   --num_layers=3 \
-   --hidden_size=256 \
-   --embedding_size=512
+
+LD_PRELOAD="/home-nfs/ttran/sw/opt/lib/libtcmalloc.so" python debug_many2one.py \
+    --data_dir ${DATA_DIR}  --train_dir ${TRAIN_DIR} \
+    --batch_size=4 \
+    --num_layers=3 \
+    --hidden_size=256 \
+    --embedding_size=512 \
+    --max_steps=60
 
